b787f05de46760571210711bf40528d0adf6c816
hackiftekhar <hack.iftekhar@gmail.com>
2023-01-20 19:47:13 +0530

    - Added swift-tools-version for Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -1,7 +1,12 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "IQPullToRefresh",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
        .library(name: "IQPullToRefresh", targets: ["IQPullToRefresh"])
    ],
